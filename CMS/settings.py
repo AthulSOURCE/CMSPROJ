98244884279979d9b219460e191f77d7fc3d6e14
TM <tmathul45@gmail.com>
2025-06-24 22:09:34 +0530

    project created!

--- CMS/settings.py
+++ CMS/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'labtechnician',
     'pharmacist',
     'reception',
+    'backend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
