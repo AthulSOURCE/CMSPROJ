481612c977959869c60642e9bd8a6330ee3d2ac7
TM <tmathul45@gmail.com>
2025-06-25 12:09:48 +0530

    addlogin1

--- CMS/settings.py
+++ CMS/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'reception',
     'backend',
     'newadmin',
-    'auth',
+    #'auth',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
--- login1/apps.py
+++ login1/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthConfig(AppConfig):
+class Login1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auth'
+    name = 'login1'
