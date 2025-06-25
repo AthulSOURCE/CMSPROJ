57333c233aa0cb7db4ed73a33328e1118d491ba5
SruthiPhilip14 <sruthimerinphilip@gmail.com>
2025-06-25 12:40:04 +0530

    added

--- CMS/settings.py
+++ CMS/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cmsproject',
         'USER':'root',
-        'PASSWORD':'athul@2002',
+        'PASSWORD':'sruthi@123',
         'HOST':'localhost',
         'PORT':3306
     }
